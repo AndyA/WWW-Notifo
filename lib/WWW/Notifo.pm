e0f8fd4797f115190d259b47bd1f0055b3ce9a93
Andy Armstrong <andy@hexten.net>
2010-07-17 20:35:32 +0100

    Bump version to 0.04

--- lib/WWW/Notifo.pm
+++ lib/WWW/Notifo.pm
@@ -15,11 +15,11 @@ WWW::Notifo - Unoffical notifo.com API
 
 =head1 VERSION
 
-This document describes WWW::Notifo version 0.03
+This document describes WWW::Notifo version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
