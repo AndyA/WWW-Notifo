f52a046dcbe7c4c4962bf35d95be78256f127bfd
Andy Armstrong <andy@hexten.net>
2010-07-18 11:09:21 +0100

    Bump version to 0.08

--- lib/WWW/Notifo.pm
+++ lib/WWW/Notifo.pm
@@ -15,11 +15,11 @@ WWW::Notifo - Interface to notifo.com notification service
 
 =head1 VERSION
 
-This document describes WWW::Notifo version 0.07
+This document describes WWW::Notifo version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 SYNOPSIS
 
